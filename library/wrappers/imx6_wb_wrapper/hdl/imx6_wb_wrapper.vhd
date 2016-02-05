4edfddba3eb7c29c47b5025b8eb7413ad3b5ab4b
Fabien Marteau <fabien.marteau@armadeus.com>
2016-02-05 14:50:26 +0100

    imx6_wb_wrapper: adding irq input

--- library/wrappers/imx6_wb_wrapper/hdl/imx6_wb_wrapper.vhd
+++ library/wrappers/imx6_wb_wrapper/hdl/imx6_wb_wrapper.vhd
@@ -21,22 +21,23 @@ library IEEE;
 Entity imx6_wb_wrapper is
 port
 (
-	refclk_clk : in std_logic;
-	hip_serial_tx_out0 : out std_logic;
-	hip_serial_rx_in0 : in std_logic;
-	npor_pin_perst : in std_logic;
-	npor_npor : in std_logic;
-	-- candroutput
-	clk_o : out std_logic;
-	rst_o : out std_logic;
-
+    refclk_clk : in std_logic;
+    hip_serial_tx_out0 : out std_logic;
+    hip_serial_rx_in0 : in std_logic;
+    npor_pin_perst : in std_logic;
+    npor_npor : in std_logic;
+    -- candroutput
+    clk_o : out std_logic;
+    rst_o : out std_logic;
+    -- irq
+    irq_irq: in std_logic_vector(15 downto 0);
     -- Wishbone interface signals
     wbm_clk : out std_logic;
     wbm_rst : out std_logic;
     wbm_address    : out std_logic_vector(11 downto 0);  -- Address bus
     wbm_readdata   :  in std_logic_vector(63 downto 0);  -- Data bus for read access
     wbm_writedata  : out std_logic_vector(63 downto 0);  -- Data bus for write access
-	wbm_byte_enable: out std_logic_vector(7 downto 0);
+    wbm_byte_enable: out std_logic_vector(7 downto 0);
     wbm_strobe     : out std_logic;                      -- Data Strobe
     wbm_write      : out std_logic;                      -- Write access
     wbm_ack        :  in std_logic;                      -- acknowledge
@@ -46,32 +47,34 @@ end entity;
 
 Architecture RTL of imx6_wb_wrapper is
 
-	component imx6_wb_wrapper_qsys
-		port (
-			refclk_clk : in std_logic;
-			hip_serial_tx_out0 : out std_logic;
-			hip_serial_rx_in0 : in std_logic;
-			npor_pin_perst : in std_logic;
-			npor_npor : in std_logic;
-			-- candroutput
-			gls_rst_reset : out std_logic;
-			gls_clk_clk : out std_logic;
-    		-- aval64
-			aval_exp_address  : out std_logic_vector(11 downto 0);
-			aval_exp_read : out std_logic;
-			aval_exp_write : out std_logic;
-			aval_exp_waitrequest : in std_logic;
-			aval_exp_byteenable : out std_logic_vector(7 downto 0);
-			aval_exp_readdatavalid : in std_logic;
-			aval_exp_writedata : out std_logic_vector(63 downto 0);
-			aval_exp_readdata : in std_logic_vector(63 downto 0)
-	);
-	end component;
-	signal clk, reset : std_logic;
-	signal aval_exp_read, aval_exp_write : std_logic;
-	signal bus_enable : std_logic;
-	signal wbm_readvalid_s : std_logic;
-	signal waitrequest_next_s : std_logic;
+    component imx6_wb_wrapper_qsys
+        port (
+            refclk_clk : in std_logic;
+            hip_serial_tx_out0 : out std_logic;
+            hip_serial_rx_in0 : in std_logic;
+            npor_pin_perst : in std_logic;
+            npor_npor : in std_logic;
+            -- candroutput
+            gls_rst_reset : out std_logic;
+            gls_clk_clk : out std_logic;
+            -- irq
+            irq_irq: in std_logic_vector(15 downto 0);
+            -- aval64
+            aval_exp_address  : out std_logic_vector(11 downto 0);
+            aval_exp_read : out std_logic;
+            aval_exp_write : out std_logic;
+            aval_exp_waitrequest : in std_logic;
+            aval_exp_byteenable : out std_logic_vector(7 downto 0);
+            aval_exp_readdatavalid : in std_logic;
+            aval_exp_writedata : out std_logic_vector(63 downto 0);
+            aval_exp_readdata : in std_logic_vector(63 downto 0)
+    );
+    end component;
+    signal clk, reset : std_logic;
+    signal aval_exp_read, aval_exp_write : std_logic;
+    signal bus_enable : std_logic;
+    signal wbm_readvalid_s : std_logic;
+    signal waitrequest_next_s : std_logic;
 begin
 
     wbm_clk <= clk;
@@ -80,7 +83,7 @@ begin
     rst_o <= reset;
 
 
-	imx6_wb_wrapperqsys : imx6_wb_wrapper_qsys
+    imx6_wb_wrapperqsys : imx6_wb_wrapper_qsys
     port map (
             -- pcie connection
             refclk_clk => refclk_clk,
@@ -91,23 +94,25 @@ begin
             -- candroutput
             gls_rst_reset => reset,
             gls_clk_clk => clk,
+            -- irq
+            irq_irq => irq_irq,
             -- aval64
-			aval_exp_waitrequest => waitrequest_next_s,
+            aval_exp_waitrequest => waitrequest_next_s,
             aval_exp_address => wbm_address,
-			aval_exp_write => aval_exp_write,
-			aval_exp_read => aval_exp_read,
-			aval_exp_byteenable => wbm_byte_enable,
-			aval_exp_readdatavalid => wbm_readvalid_s,
+            aval_exp_write => aval_exp_write,
+            aval_exp_read => aval_exp_read,
+            aval_exp_byteenable => wbm_byte_enable,
+            aval_exp_readdatavalid => wbm_readvalid_s,
             aval_exp_writedata => wbm_writedata,
             aval_exp_readdata => wbm_readdata
-	);
+    );
 
-	waitrequest_next_s <= '1' when (bus_enable ='1' and wbm_ack = '0') else '0';
+    waitrequest_next_s <= '1' when (bus_enable ='1' and wbm_ack = '0') else '0';
 
-	wbm_readvalid_s <= (aval_exp_read and wbm_ack);
-	bus_enable <= aval_exp_read or aval_exp_write;
-	wbm_cycle  <= bus_enable;
-	wbm_strobe <= bus_enable;
-	wbm_write  <= bus_enable and aval_exp_write;
+    wbm_readvalid_s <= (aval_exp_read and wbm_ack);
+    bus_enable <= aval_exp_read or aval_exp_write;
+    wbm_cycle  <= bus_enable;
+    wbm_strobe <= bus_enable;
+    wbm_write  <= bus_enable and aval_exp_write;
 
 end architecture RTL;
